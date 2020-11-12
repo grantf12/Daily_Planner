8be21dbb3dc814607f6c8e989e4d16ff540be7aa
Grant Ferment <grantferment@gmail.com>
2020-11-11 19:01:45 -0500

    working on on.click function

--- index.js
+++ index.js
@@ -4,32 +4,22 @@ $(document).ready(function() {
     // $("#currentDay").text(now.format("dddd, MMMM Do YYYY"))
     // console.log(now.format("dddd, MMMM Do YYYY"));
     // var currentTime = now.format("HH")
-    // for (var i = 0; i < 8; i++) {
-    //     // if (currentTime < "data-hour") {
-    //     //     $("textarea").addClass("past")
-    //     // }
-    //     if (currentTime > "data-hour") {
-    //         $("textarea").addClass("future")
-    //     }
-    //     // if (currentTime = "data-hour") {
-    //     //     $("textarea").addClass("present")
-    //     // }
-    // }
-    // // format is a function in moment that will let you format the info
-    // // moment.com go to Docs, then Display on the right to see how you would want the date displayed 
-    // //              console.log(parseIn(now.format("H")));
-    // // Will display time in military time as a string (if you parseIn it becomes a number)
+    // function what
     
     
     // Add click listener that will:
         // description added to localStorage
         // 
     // localStorage.setItem()
+
 $("#saveBtnNine").on("click", function() {
     var timeNine = document.getElementById("nineAM");
-    var descriptionNine = document.getElementById("nine");
-    localStorage.setItem(timeNine, descriptionNine)
+    var descriptionNine = document.getElementsByClassName("descriptionNine");
+    localStorage.setItem("appointmentNine", descriptionNine)
+
+
+$("#nineAM .description").val(localStorage.getItem("nineAM"))
 })
-$("#nineAM").val(localStorage.getItem(timeNine))
+// descriptionNine = localStorage.getItem("appointmentNine")
 
 })
