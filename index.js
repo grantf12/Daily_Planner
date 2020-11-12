0030106d97f862250cabf4ee9fb7cc7dab68374f
Grant Ferment <grantferment@gmail.com>
2020-11-11 22:19:05 -0500

    added for loop, changed id's and fixed saveBtn

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // Using moment(), this function calls for the day, month, and year. 
+    var now = moment()
+    $("#currentDay").text(now.format("dddd, MMMM Do YYYY"))
 
-    // var now = moment()
-    // $("#currentDay").text(now.format("dddd, MMMM Do YYYY"))
-    // console.log(now.format("dddd, MMMM Do YYYY"));
-    // var currentTime = now.format("HH")
-    // function what
-    
-    
-    // Add click listener that will:
-        // description added to localStorage
-        // 
-    // localStorage.setItem()
+    highlighter();
 
-$("#saveBtnNine").on("click", function() {
-    var timeNine = document.getElementById("nineAM");
-    var descriptionNine = document.getElementsByClassName("descriptionNine");
-    localStorage.setItem("appointmentNine", descriptionNine)
+    // Highlights each time-block for Past, Present and Future by calling for the hour in military time and checks it against the id's
+    function highlighter() {
+        var hour = now.format("HH")
+        $("textarea").each(function () {
+            var current = parseInt($(this).attr("id"));
+            if (current > hour) {
+                $(this).addClass("future")
+            }
+            else if (current === hour) {
+                $(this).addClass("present")
+            }
+            else {
+                $(this).addClass("past")
+            }
+        })
+    }
 
+    // For loop that loops through the different appointments that are saved in the localStorage and makes a text in textarea[i]
+    for (var i = 9; i < 18; i++) {
+        var savedAppointment = localStorage.getItem("appointment" + (i));
+        if (savedAppointment !== null) {
+            $("#" + (i)).text(savedAppointment);
+        }
+    }
 
-$("#nineAM .description").val(localStorage.getItem("nineAM"))
-})
-// descriptionNine = localStorage.getItem("appointmentNine")
+    // Takes the data-btn # from the attributes in the button that is "clicked". Then takes the value that in in the textarea using the $id by using the data-btn number then saves that in localStorage
+    $(".saveBtn").on("click", function () {
+        var saved = $(this).attr("data-btn");
+        var text = $("#" + saved).val();
+        localStorage.setItem("appointment" + saved, text)
 
+    })
 })
